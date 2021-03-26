e96fa7e45ad6cee6093265b5ccb48f30719ea86b
Albert J. Williams <albertwilliams.xyz@gmail.com>
2021-03-26 12:58:48 -0400

    Trying to update to use recursive functions

--- src/main/java/com/search_engine/App.java
+++ src/main/java/com/search_engine/App.java
@@ -1,28 +1,45 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-import java.io.IOException;
 
 class App {
 	public static void main(String[] args) throws IOException {
 		try {
 			String[] rootURLs = {
 				"https://en.wikipedia.org/",
-				"https://www.reddit.com/",
+				// "https://www.reddit.com/",
 			};
-			String[] urls = getListOfURLs(rootURLs);
+			for (String rootURL: rootURLs) {
+				ArrayList<String> listOfURLs = getListOfURLs(rootURL, 1, new ArrayList<String>());
+				System.out.println("Imprimiendo respuestas");
+				for (String url: listOfURLs) {
+				}
+			}
 		} catch (Exception e) {
-			System.out.println(e);
 		}
 	}
 
-	public static String[] getListOfURLs(String[] rootURLs) throws IOException {
-		for (String url: rootURLs) {
+	public static ArrayList<String> getListOfURLs(String url, int depth, ArrayList<String> listOfURLs) throws IOException {
+		System.out.println("----------------------------------------------------------------------------------------------------");
+		System.out.println("Entrando en getListOfURLs. url: " + url + " depth: " + depth);
+		if (depth > 0) {
 			Document document = Jsoup.connect(url).get();
-			System.out.println(document.title());
-		} 
-		return rootURLs;
+			Elements hyperlinks = document.select("a[href]");
+			for (Element hyperlink: hyperlinks) {
+				String href = hyperlink.attr("href");
+				if (href.startsWith("https")) {
+					System.out.println("href: " + href);
+					ArrayList<String> response = getListOfURLs(href, depth - 1, listOfURLs);
+					listOfURLs.addAll(response);
+				}
+			}
+		}
+		System.out.println(listOfURLs.size());
+		return listOfURLs;
 	}
 }
